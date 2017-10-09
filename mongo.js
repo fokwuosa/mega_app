6f8891221d8681d471256fb82953be701f419b6f
Faith Okwuosa <faith.okwuosa@MAC000360.local>
2017-10-09 16:53:51 +0100

    Fixed async bug with DB connection

--- mongo.js
+++ mongo.js
@@ -5,33 +5,40 @@ const url = 'mongodb://localhost:27017/users';
 let db;
 
 function addUser(name, cb){
-    if (!db) {
-        initDB();
-    }
-    db.collection('users').insert( { name: name} );
-    console.log("Added name: " + name);
-
-    cb();
-}
-
-function getUsers(cb){
-    if (!db) {
-        initDB();
-    }
-    db.collection('users').find({}).toArray(function (err, users) {
-        if (err) return console.log(err);
-
-        cb(users);
+    
+    getConnection().then((db) => {
+        db.collection('users').insert( { name: name} );
+        console.log("Added name: " + name);
+        cb();
     })
 }
 
-function initDB(){
-    MongoClient.connect(url, function(err, database) {
-        if (err) return console.log(err);
-        console.log("Connected successfully to server");
+function getUsers(cb){
     
-        db = database;
-    });
+    getConnection().then((db) => {
+        db.collection('users').find({}).toArray(function (err, users) {
+            if (err) return console.log(err);
+            cb(users);
+        })
+    })
+}
+
+
+function getConnection(){
+    if(db) {
+        return Promise.resolve(db)
+    }
+
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(url, function(err, database) {
+            if (err) return reject(err);
+            console.log("Connected successfully to server");
+            //return resolve(random)
+            db = database
+            return resolve(database)
+        });
+    })
+    
 }
 
 module.exports = {

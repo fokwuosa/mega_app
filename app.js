d62abf2491e3202bd4989900c382a6883d78ab40
Faith Okwuosa <faith.okwuosa@MAC000360.local>
2017-10-09 16:36:27 +0100

    Added JSON Api support

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const app = express()
 app.set('view engine', 'pug')
 
 var bodyParser = require('body-parser')
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({    
   extended: true
 })); 
 
+app.use(express.json());
 app.use(express.urlencoded());
 
 app.get('/users', function (req, res) {
@@ -22,8 +24,22 @@ app.get('/users', function (req, res) {
     });
 })
 
+app.get('/api/users', function (req, res) {
+    
+    mongo.getUsers(function (users){
+        res.json(users);
+    });
+})
+
 app.post('/user', function(req, res){
 
+    mongo.addUser(req.body.name, function() {
+        res.redirect('/users');
+    });
+})
+
+app.post('/api/user', function(req, res){
+    
     mongo.addUser(req.body.name, function() {
         res.redirect('/users');
     });
